26c500ca5c4cd055e89f2479167ffa8149707076
Nick Gastis <nickgastis1@gmail.com>
2023-04-04 16:51:45 -0400

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = sorted([character for character in word])
+
+    def match(self, word_list):
+        matches = []
+
+        for word in word_list:
+            if sorted([letter for letter in word]) == self.word:
+                matches.append(word)
+        
+        return matches
